7924fed1af653f47ba9d861289c2d2c58751fcdf
FOS-Intern <elbertchailes888@gmail.com>
2024-09-26 09:37:14 +0700

    fix: input error build

---
+++ app/investor-view/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";npx shadcn@latest add input
+
+function page() {
+  return (
+    <div className="">
+      <div className="w-[80vw] bg-white h-8 rounded-xl flex items-center">
+        
+      </div>
+    </div>
+  );
+}
+
+export default page;
