594e8978dc78cc4903f932db4177ccf7314fc3c9
FOS-Intern <elbertchailes888@gmail.com>
2024-09-26 09:39:11 +0700

    fix: build

--- app/investor-view/page.tsx
+++ app/investor-view/page.tsx
@@ -1,11 +1,9 @@
-import React from "react";npx shadcn@latest add input
+import React from "react";
 
 function page() {
   return (
     <div className="">
-      <div className="w-[80vw] bg-white h-8 rounded-xl flex items-center">
-        
-      </div>
+      <div className="w-[80vw] bg-white h-8 rounded-xl flex items-center"></div>
     </div>
   );
 }
