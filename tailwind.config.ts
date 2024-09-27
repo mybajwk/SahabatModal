65c1c4acd2f0c5d9126a3f9f8482d63ea270960c
Mesach Harmasendro <13522117@std.stei.itb.ac.id>
2024-09-27 12:50:15 +0700

    feat: change tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,6 +51,10 @@ const config: Config = {
           "5": "hsl(var(--chart-5))",
         },
       },
+      dropShadow: {
+        "text-white":
+          "0px 0px 28.792px rgba(255, 255, 255, 0.40), 0px 0px 14.396px rgba(255, 255, 255, 0.80)",
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
@@ -60,8 +64,25 @@ const config: Config = {
         lexend: ["var(--font-lexend)"],
         bricolage: ["var(--font-bricolage)"],
       },
+      backgroundImage: {
+        "blue-gradient-radial": "radial-gradient(circle, #3E3183, #3B47BC)",
+        "green-gradient":
+          "radial-gradient(61.94% 48.96% at 49.96% 96.22%, #04C782 0%, #18D3A7 100%)",
+      },
+      boxShadow: {
+        "custom-inset":
+          "0px 2.676px 3.842px -2.47px rgba(116, 163, 255, 0.30) inset, 0px 0.48px 0.755px -0.274px #FFF inset, 0px -5.626px 4.665px -4.391px rgba(96, 68, 144, 0.30) inset, 0px 6.724px 6.861px -3.293px rgba(202, 172, 255, 0.30) inset, 0px 0.274px 1.235px 2.058px rgba(154, 146, 210, 0.25) inset, 0px 0.069px 2.744px 0px rgba(227, 222, 255, 0.20) inset",
+        //button hijau
+        "custom-shadow-green-button":
+          "0px 1.305px 81.553px 0px #D9E6FE, 0px 0px 0px 14.353px rgba(255, 255, 255, 0.07), 0px -1.305px 0px 3.262px rgba(0, 0, 0, 0.20) inset, 0px 1.305px 0px 3.262px rgba(255, 255, 255, 0.40) inset",
+        //button biru
+        "custom-shadow-blue-button":
+          "0px 1.079px 67.417px 0px #D9E6FE, 0px 0px 0px 11.865px rgba(255, 255, 255, 0.07), 0px -1.079px 0px 2.697px rgba(0, 0, 0, 0.20) inset, 0px 1.079px 0px 2.697px rgba(255, 255, 255, 0.40) inset",
+      },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+  ],
 };
 export default config;
