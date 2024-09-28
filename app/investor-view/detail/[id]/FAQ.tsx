d873ff63e32abb1a42034156f4017156e94ad950
FOS-Intern <elbertchailes888@gmail.com>
2024-09-28 18:16:35 +0700

    feat: faq subpage

---
+++ app/investor-view/detail/[id]/FAQ.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import FAQCard from "./FAQCard";
+
+function FAQ() {
+  const faqData = [
+    {
+      question: "What is Lorem Ipsum?",
+      answer:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      question: "Why use Lorem Ipsum?",
+      answer:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    },
+    {
+      question: "Where does it come from?",
+      answer:
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+    },
+    {
+      question: "How many variations exist?",
+      answer:
+        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.",
+    },
+    {
+      question: "Where can I get some?",
+      answer:
+        "There are many websites on the Internet that provide Lorem Ipsum passages, and many variants of the text can be found by performing a simple search.",
+    },
+  ];
+
+  return (
+    <div className="p-6 w-full font-lexend flex flex-col space-y-4 items-center">
+      <h1 className="font-bold text-white drop-shadow-text-white">
+        Frequently Asked Question
+      </h1>
+      {faqData.map((item, index) => (
+        <FAQCard key={index} question={item.question} answer={item.answer} />
+      ))}
+    </div>
+  );
+}
+
+export default FAQ;
--- app/investor-view/detail/[id]/page.tsx
+++ app/investor-view/detail/[id]/page.tsx
@@ -1,9 +1,76 @@
+"use client";
+
+import React, { useState } from "react";
+import Deskripsi from "./Deskripsi";
+import Reward from "./Reward";
+import FAQ from "./FAQ";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function InvestorDetail({ params }: { params: { id: string } }) {
-  const { id } = params;
+  // const { id } = params;
+  const [activeTab, setActiveTab] = useState("Deskripsi");
+
+  const handleTabClick = (tabName: string) => {
+    setActiveTab(tabName);
+  };
+
+  const renderActiveTab = () => {
+    switch (activeTab) {
+      case "Deskripsi":
+        return <Deskripsi />;
+      case "Reward":
+        return <Reward />;
+      case "FAQ":
+        return <FAQ />;
+      default:
+        return <Deskripsi />;
+    }
+  };
+
   return (
-    <div className="min-h-screen w-full bg-conic-blue">
-      {/* Rendering the ID to ensure it's correctly fetched */}
-      <h1>Investor ID: {id}</h1>
+    <div className="relative bg-conic-blue font-lexend flex flex-col items-center">
+      <div className="flex flex-col items-center space-y-5 p-16">
+        <video width="240" height="320" controls>
+          <source src="@/app/assets/video.mp4" type="video/mp4" />
+        </video>
+        <div className="flex flex-col items-start space-y-3 border-l-4 border-l-[#18D3A7] pl-4">
+          <div className="flex flex-col space-y-1">
+            <h1 className="text-[#18D3A7] font-semibold">Rp20.000.000,00</h1>
+            <p className="text-[10px]">
+              dana yang terkumpul dari total Rp100.000.000,00
+            </p>
+          </div>
+          <div className="flex flex-col space-y-1">
+            <h1 className="text-[#18D3A7] font-semibold">10</h1>
+            <p className="text-[10px]">investor</p>
+          </div>
+          <div className="flex flex-col space-y-1">
+            <h1 className="text-[#18D3A7] font-semibold">10</h1>
+            <p className="text-[10px]">hari tersisa</p>
+          </div>
+        </div>
+      </div>
+      <div className="relative bg-white w-full flex flex-row space-x-6 text-black font-light justify-center items-center py-2">
+        {["Deskripsi", "Reward", "FAQ"].map((tab) => (
+          <p
+            key={tab}
+            className={`relative cursor-pointer px-2 py-1`}
+            onClick={() => handleTabClick(tab)}
+          >
+            {tab}
+            <span
+              className={`absolute inset-x-0 bottom-0 mx-auto h-[2px] bg-black transform scale-x-0 transition-transform duration-300 ease-out ${activeTab === tab ? "bg-black scale-x-100" : ""}`}
+              style={
+                activeTab === tab ? {} : { transformOrigin: "center bottom" }
+              }
+            ></span>
+          </p>
+        ))}
+      </div>
+      <div className="w-full max-w-[900px] flex justify-center">
+        {" "}
+        {renderActiveTab()}
+      </div>
     </div>
   );
 }
