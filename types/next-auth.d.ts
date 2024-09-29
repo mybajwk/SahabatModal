6104b883a78d87283105a9de2421cb286191af0f
Enrique Yanuar <enriqueyanuar.7@gmail.com>
2024-09-29 12:24:07 +0700

    fix

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,31 +1,35 @@
-import { DefaultSession } from "next-auth";
+// types/next-auth.d.ts
+import "next-auth";
 
 declare module "next-auth" {
+  /**
+   * Extends the built-in session/user types to include user role
+   */
+
   interface User {
     id: string;
-    role?: string;
+    name: string;
+    email: string;
+    password: string;
+    created_at: Date;
+    phone_number: string;
+    username: string;
+    role: number; // Ensure the type matches your database schema and user object
+    image: string | null;
   }
 
   interface Session {
-    user: DefaultSession["user"] & {
-      id: string;
-      role?: string;
-    };
+    user: {
+      role?: number;
+    } & DefaultSession["user"];
   }
 }
 
-import { DefaultSession } from "next-auth";
-
-declare module "next-auth" {
-  interface User {
-    id: string;
-    role?: string;
-  }
-
-  interface Session {
-    user: DefaultSession["user"] & {
-      id: string;
-      role?: string;
-    };
+declare module "next-auth/jwt" {
+  /**
+   * Extends the JWT type to include user role
+   */
+  interface JWT {
+    role?: number;
   }
 }
