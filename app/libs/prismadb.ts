90596821b0681213d586799b3d74f2e7af4c382a
Enrique Yanuar <enriqueyanuar.7@gmail.com>
2024-09-29 12:04:24 +0700

    Update prismadb.ts

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
+  // Change `var` to `let` for better scoping rules
   var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+// Use `const` here since the client itself will not be reassigned
+const client = global.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") global.prisma = client;
 
 export default client;
