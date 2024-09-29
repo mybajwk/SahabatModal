6615c2c784d035c5f232e429be6bf270cc0d32f1
Enrique Yanuar <enriqueyanuar.7@gmail.com>
2024-09-29 12:13:06 +0700

    Update prismadb.ts

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -8,8 +8,8 @@ declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const client = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = client;
 
-export const client = prisma;
+export default client;
