a9489f1744466b777a006345a4770582da01e299
FOS-Intern <elbertchailes888@gmail.com>
2024-10-01 21:54:49 +0700

    feat: add feeds

--- app/forum/page.tsx
+++ app/forum/page.tsx
@@ -3,7 +3,34 @@
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import FeedCard from "./FeedCard";
-import SearchBar from "@/components/ui/searchbar";
+import { FaPlus } from "react-icons/fa";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 const dummyData = [
   {
@@ -127,10 +154,106 @@ const dummyData = [
     ],
   },
 ];
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title must not be empty.",
+  }),
+  content: z.string().min(1, {
+    message: "Content must not be empty.",
+  }),
+  tags: z.string().min(1, {
+    message: "At least 1 tag.",
+  }),
+});
+
 function ForumPage() {
-  const [query, setQuery] = useState<string>("");
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+      tags: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <div className="bg-white min-h-screen w-full flex justify-center">
+    <div className="bg-white min-h-screen w-full flex justify-center relative">
+      <Dialog>
+        <DialogTrigger className="fixed opacity-65 hover:opacity-100 bottom-5 right-5 bg-[#18D3A7] w-24 p-7 aspect-square rounded-full hover:cursor-pointer flex justify-center items-center">
+          <FaPlus className="text-white w-full h-auto aspect-square" />
+        </DialogTrigger>
+        <DialogContent className="bg-white text-black font-lexend w-[90%] rounded-lg">
+          <DialogHeader className="space-y-7">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <DialogTitle>Post Feeds</DialogTitle>
+                <DialogDescription className="flex flex-col space-y-3">
+                  <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col space-y-1 items-start">
+                        <FormLabel>Title</FormLabel>
+                        <FormControl>
+                          <Input placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <div className="flex flex-col space-y-2">
+                    <FormField
+                      control={form.control}
+                      name="content"
+                      render={({ field }) => (
+                        <FormItem className="flex flex-col space-y-1 items-start">
+                          <FormLabel>Content</FormLabel>
+                          <FormControl>
+                            <Textarea placeholder="" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+
+                  <div className="flex flex-col space-y-2">
+                    <FormField
+                      control={form.control}
+                      name="tags"
+                      render={({ field }) => (
+                        <FormItem className="flex flex-col space-y-1 items-start">
+                          <FormLabel>Tags (Ex: Finance, Marketing)</FormLabel>
+                          <FormControl>
+                            <Input placeholder="" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </DialogDescription>
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button type="submit" className="bg-[#18D3A7]">
+                      Post
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </form>
+            </Form>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
       <div className="w-[90%] p-4 space-y-5 flex-col items-center max-w-[600px]">
         <div className="flex flex-col space-y-2">
           {/* <SearchBar onSearchChange={setQuery} /> */}
