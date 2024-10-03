b9b92edbc5f104797d39d7b8d9c57ad964c8f4a4
FOS-Intern <elbertchailes888@gmail.com>
2024-10-03 10:13:27 +0700

    fix: reponsive landing page

--- app/investor-view/detail/[id]/FAQCard.tsx
+++ app/investor-view/detail/[id]/FAQCard.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import {
-  Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
@@ -10,24 +9,19 @@ import {
 interface FAQCardProps {
   question: string;
   answer: string;
+  index: number;
 }
 
-const FAQCard: React.FC<FAQCardProps> = ({ question, answer }) => {
+const FAQCard: React.FC<FAQCardProps> = ({ question, answer, index }) => {
   return (
-    <Accordion
-      type="single"
-      collapsible
-      className="w-full rounded-md border border-[#D9D9D9] p-3 shadow-custom-inset"
-    >
-      <AccordionItem value="item-1" className="border-b-0 ">
-        <AccordionTrigger className="py-1 text-justify">
-          {question}
-        </AccordionTrigger>
-        <AccordionContent className="bg-white rounded-md text-black px-4 py-3">
-          {answer}
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+    <AccordionItem value={`item-${index + 1}`} className="border-b-0">
+      <AccordionTrigger className="w-full sm:text-lg rounded-md border border-[#D9D9D9] p-3 shadow-custom-inset">
+        {question}
+      </AccordionTrigger>
+      <AccordionContent className="bg-white sm:text-sm rounded-md text-black px-4 py-3">
+        {answer}
+      </AccordionContent>
+    </AccordionItem>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,161 +1,254 @@
-import GetStartedButton from "../components/ui/get-started-button";
-import StartButton from "../components/ui/start-button";
-import StartButton2 from "../components/ui/start-button-2";
-import InspiringSection from "./inspiring-section";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import peopleThumbnail from "@/public/people.png";
+import verticalWay from "@/public/vertical-way.png";
+import horizontalWay from "@/public/horizontal-way.png";
+import forumPNG from "@/public/forum.png";
+import FAQCard from "./investor-view/detail/[id]/FAQCard";
+import useWindowSize from "@/hooks/useWindowSize";
+import { Accordion } from "@/components/ui/accordion";
 
 export default function Home() {
+  const benefitSeeker = [
+    {
+      number: "01",
+      title: "Crowdfunding",
+      description:
+        "Crowdfunding memungkinkan pengumpulan dana untuk seeker oleh banyak investor dengan reward dan perjanjian yang ditentukan oleh seeker",
+    },
+    {
+      number: "02",
+      title: "Mentoring",
+      description:
+        "Mentoring memberikan bantuan kepada seeker dalam hal konsultasi bisnis dengan para ahli di bidangnya.",
+    },
+    {
+      number: "03",
+      title: "Networking",
+      description:
+        "Networking memungkinkan seeker untuk memperluas koneksi bisnisnya dengan bertemu banyak pihak yang tertarik dengan bisnis mereka.",
+    },
+  ];
+  const benefitInvestor = [
+    {
+      number: "01",
+      title: "Pilihan Investasi",
+      description:
+        "Dengan SahabatModal, para investor lebih mudah melakukan investasi pada seeker yang diminati sesuai dengan profil risiko masing-masing",
+    },
+    {
+      number: "02",
+      title: "Jaringan koneksi",
+      description:
+        "Dengan sistem forum pada SahabatModal, para investor akan lebih mudah mencari koneksi, baik dengan sesama investor maupun dengan seeker",
+    },
+    {
+      number: "03",
+      title: "Investasi terpercaya",
+      description:
+        "Sistem inkubasi pada SahabatModal akan menjamin komitmen yang dimiliki oleh seeker, sehingga investor dapat berinvestasi dengan tenang",
+    },
+  ];
+
+  const faqData = [
+    {
+      question: "What is Lorem Ipsum?",
+      answer:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      question: "Why use Lorem Ipsum?",
+      answer:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    },
+    {
+      question: "Where does it come from?",
+      answer:
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+    },
+    {
+      question: "How many variations exist?",
+      answer:
+        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.",
+    },
+    {
+      question: "Where can I get some?",
+      answer:
+        "There are many websites on the Internet that provide Lorem Ipsum passages, and many variants of the text can be found by performing a simple search.",
+    },
+  ];
+
+  const { width } = useWindowSize();
+
   return (
-    <>
-      <section
-        className="min-h-screen bg-cover bg-center text-white"
+    <div
+      className="flex min-h-screen w-full flex-col font-bricolage items-center"
+      style={{
+        backgroundImage: 'url("./bg_main.png")',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="flex flex-col sm:flex-row-reverse sm:space-x-8 sm:pr-8 sm:pl-0 items-center px-8 pb-12 md:pt-12 max-w-[1024px]">
+        <Image
+          src={peopleThumbnail}
+          alt="thumbnail"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="rounded-xl object-contain object-center aspect-square sm:ml-8 w-full"
+        />
+        <div className="flex flex-col space-y-3 md:space-y-10 items-start">
+          <h1 className="font-semibold text-2xl sm:text-3xl lg:text-4xl text-center sm:text-start md:pr-24">
+            Beri Solusi Melalui Parsial Investasi dan Inkubasi bagi UMKM dan
+            Startup.
+          </h1>
+          <Button className="shadow-custom-bubble bg-transparent lg:text-2xl lg:py-10 rounded-full w-fit place-self-center sm:place-self-start mt-8 px-12 py-5">
+            GET STARTED
+          </Button>
+        </div>
+      </div>
+      <div
+        className="w-full h-[100px]"
         style={{
-          backgroundImage: "url('/BG.png')",
+          backgroundImage: 'url("./iklan.png")',
           backgroundSize: "cover",
-          backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
         }}
+      ></div>
+      <div
+        style={{
+          backgroundImage: 'url("./bg_main.png")',
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+        className="flex flex-col items-center w-full space-y-8 py-12 lg:py-32"
       >
-        <div
-          className="container mx-auto px-4 py-20"
-          style={{ paddingTop: "20rem", marginLeft: "7rem" }}
-        >
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-            <div className="space-y-6">
-              <h1
-                className="text-7xl font-bold leading-tight mb-6"
-                style={{ fontFamily: "Lexend, sans-serif" }}
-              >
-                Beri Solusi Melalui Parsial Investasi dan Inkubasi bagi UMKM dan
-                Startup.
-              </h1>
-              <GetStartedButton />
-            </div>
+        <h1 className="font-semibold text-2xl lg:text-4xl text-center">
+          Bagaimana Cara Kerjanya?
+        </h1>
+        {width && width < 640 && (
+          <Image
+            src={verticalWay}
+            alt="thumbnail"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="object-cover w-1/3 object-center"
+          />
+        )}
+        {width && width >= 640 && (
+          <Image
+            src={horizontalWay}
+            alt="thumbnail"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="object-cover w-4/5 object-center max-w-[1024px]"
+          />
+        )}
+      </div>
+      <div className="bg-[#3E3183E5] w-full flex flex-col items-center space-y-4 px-8 md:py-12">
+        <div className="flex flex-col sm:flex-row sm:space-y-0 sm:space-x-8 sm:items-center space-y-4 py-8 max-w-[1024px]">
+          <div className="flex flex-col space-y-4 items-center sm:items-start">
+            <h2 className="font-semibold text-2xl lg:text-3xl text-center sm:text-start">
+              Apa Benefit Yang Didapat Oleh Seeker?
+            </h2>
+            <p className="text-center sm:text-start text-xs lg:text-sm">
+              Buka Funding Untuk bisnismu sekarang!!
+            </p>
+            <Button className="bg-green-gradient rounded-full lg:text-xl px-12 py-6 shadow-custom-chill-green-button">
+              START
+            </Button>
+          </div>
+          <div className="flex flex-col space-y-6 py-6 shadow-custom-inset rounded-lg pr-4">
+            {benefitSeeker.map((benefit, index) => (
+              <div key={index} className="flex flex-row space-x-3">
+                <div className="rounded-r-lg flex justify-center pt-4">
+                  <p className="bg-white px-4 py-2 rounded-r-lg font-lexend h-fit text-black font-bold">
+                    {benefit.number}
+                  </p>
+                </div>
+                <div className="flex flex-col space-y-1 lg:text-lg">
+                  <h3>{benefit.title}</h3>
+                  <p className="text-[10px] font-poppins font-light lg:text-[12px]">
+                    {benefit.description}
+                  </p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-      </section>
-
-      <section className="bg-white py-0">
-        <div className="w-screen overflow-hidden">
-          {/* <Image
-            src="/iklan.png"
-            alt="Iklan Promo"
-            className="w-screen h-auto"
-            width={500}
-            height={500}
-            style={{
-              objectFit: "cover",
-            }}
-          /> */}
+        <div className="flex flex-col sm:flex-row-reverse sm:space-y-0 sm:items-center space-y-4 py-8 max-w-[1024px]">
+          <div className="flex flex-col space-y-4 items-center sm:items-start sm:ml-8">
+            <h2 className="font-semibold text-2xl lg:text-3xl text-center sm:text-end">
+              Apa Benefit Yang Didapat Oleh Investor?
+            </h2>
+          </div>
+          <div className="flex flex-col space-y-6 py-6 shadow-custom-inset rounded-lg pr-4">
+            {benefitInvestor.map((benefit, index) => (
+              <div key={index} className="flex flex-row space-x-3">
+                <div className="rounded-r-lg flex justify-center pt-4">
+                  <p className="bg-white px-4 py-2 rounded-r-lg font-lexend h-fit text-black font-bold">
+                    {benefit.number}
+                  </p>
+                </div>
+                <div className="flex flex-col space-y-1 lg:text-lg">
+                  <h3>{benefit.title}</h3>
+                  <p className="text-[10px] font-poppins font-light lg:text-[12px]">
+                    {benefit.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </section>
-
-      <section
-        className="bg-blue-900 text-white py-20"
+      </div>
+      <div className="bg-white w-full py-8 lg:py-16 flex flex-col items-center">
+        <Image
+          src={forumPNG}
+          alt="thumbnail"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="object-cover w-full object-center max-w-[1024px]"
+        />
+      </div>
+      <div
         style={{
-          backgroundImage: "url('/cara_kerja.png')",
+          backgroundImage: 'url("./faq_bg.png")',
           backgroundSize: "cover",
           backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          minHeight: "70vh",
         }}
+        className="w-full flex flex-col items-center"
       >
-        <div className="container mx-auto text-center">
-          <h2
-            className="text-7xl font-bold mt-3 mb-12 text-left -ml-20"
-            style={{
-              fontFamily: "Lexend, sans-serif",
-              textShadow: "0 0 15px rgba(255, 255, 255, 0.8)",
-            }}
-          >
-            Bagaimana Cara Kerjanya?
-          </h2>
+        <div className="max-w-[1024px] flex w-full flex-col md:flex-row md:space-y-0 md:px-8 md:space-x-20 items-center space-y-8 py-12 min-h-[550px]">
+          <h1 className="font-semibold text-2xl text-center">
+            Frequently Asked Question
+          </h1>
+          <div className="flex flex-col space-y-5 w-[80%]">
+            <Accordion
+              type="single"
+              collapsible
+              className="flex flex-col space-y-6"
+            >
+              {faqData.map((item, index) => (
+                <FAQCard
+                  key={index}
+                  question={item.question}
+                  answer={item.answer}
+                  index={index}
+                />
+              ))}
+            </Accordion>
+          </div>
         </div>
-      </section>
-      <section
-        className="min-h-screen bg-cover bg-center text-white"
-        style={{
-          backgroundImage: "url('/benefit.png')", // Ensure correct path for background image
-          backgroundSize: "cover", // Cover the section, ensure the background stretches
-          backgroundPosition: "top", // Center the image
-          backgroundRepeat: "no-repeat", // Prevent background from repeating
-          backgroundAttachment: "scroll", // Background scrolls with the content
-        }}
-      >
-        <div className="container mx-auto py-20">
-          <h2
-            className="text-7xl font-bold mt-80 -mb-10 -ml-20 text-left"
-            style={{
-              fontFamily: "Lexend, sans-serif",
-              textShadow: "0 0 15px rgba(255, 255, 255, 0.8)", // Glow effect
-            }}
-          >
-            Apa Benefit Yang
-          </h2>
-        </div>
-        <div className="container mx-auto py-20">
-          <h2
-            className="text-7xl font-bold -mt-20 -mb-10 -ml-20 text-left"
-            style={{
-              fontFamily: "Lexend, sans-serif",
-              textShadow: "0 0 15px rgba(255, 255, 255, 0.8)", // Glow effect
-            }}
-          >
-            Didapat Oleh Seeker?
-          </h2>
-        </div>
-
-        <h1 className="text-10xl font-bold mb-12"></h1>
-        <StartButton />
-        <div className="container mx-auto py-20">
-          {/* Title with Glow Effect */}
-          <h2
-            className="text-7xl font-bold mt-96 -mb-10 -mr-40 text-right"
-            style={{
-              fontFamily: "Lexend, sans-serif",
-              textShadow: "0 0 15px rgba(255, 255, 255, 0.8)", // Glow effect
-            }}
-          >
-            Apa Benefit Yang
-          </h2>
-        </div>
-        <div className="container mx-auto py-20">
-          <h2
-            className="text-7xl font-bold -mt-20 -mb-10 -mr-40 text-right"
-            style={{
-              fontFamily: "Lexend, sans-serif",
-              textShadow: "0 0 15px rgba(255, 255, 255, 0.8)", // Glow effect
-            }}
-          >
-            Didapat Oleh Investor?
-          </h2>
-        </div>
-        <StartButton2 />
-      </section>
-      <section className="bg-white py-0">
-        <div className="w-screen overflow-hidden">
-          {/* <Image
-            src="/forum.png"
-            alt="Forum"
-            className="w-screen h-auto"
-            width={500}
-            height={500}
-            style={{
-              objectFit: "cover",
-            }}
-          /> */}
-        </div>
-      </section>
-      <section
-        className="bg-blue-900 text-white py-20" // Remove extra padding
-        style={{
-          backgroundImage: "url('/faq.png')",
-          backgroundSize: "cover", // Change to 'contain' so the image fits within the section without cropping
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          minHeight: "70vh", // Adjust height to ensure the section has enough height without whitespace
-        }}
-      ></section>
-      <InspiringSection />
-    </>
+      </div>
+    </div>
   );
 }
