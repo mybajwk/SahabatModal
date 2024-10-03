cfb83e01dd6030f859c6cf0da805719ba1a90bc5
Enrique Yanuar <enriqueyanuar.7@gmail.com>
2024-10-03 19:10:38 +0700

    Revert "Merge branch 'main' into develop"
    This reverts commit 23294d7bfd0ed3dfdd06b0ca7eba91cc7abafb0e, reversing
    changes made to c5e8711d6d8da554f23b4f9531885fdf738394c9.

---
+++ components/ui/start-button-2.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const StartButton2 = () => {
+  return (
+    <div className="text-right">
+      {" "}
+      {/* Align the button to the right */}
+      <button
+        className="text-white font-bold py-5 px-20 mr-20 mb-30 rounded-full"
+        style={{
+          background: "linear-gradient(180deg, #2ED3B7 0%, #00B894 100%)", // Gradient background
+          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)", // Add shadow for depth
+          border: "3px solid #00F0D5", // Border around the button
+          marginBottom: "10rem",
+        }}
+      >
+        START
+      </button>
+    </div>
+  );
+};
+
+export default StartButton2;
