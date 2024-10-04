58e030b4046d9cda9f634a153300130bd5b5bbd3
Lint Action <lint-action@samuelmeuli.com>
2024-10-04 07:14:33 +0000

    Fix code style issues with Prettier

--- app/api/mentoring/route.ts
+++ app/api/mentoring/route.ts
@@ -58,13 +58,13 @@ export async function POST(req: NextRequest) {
         data: newMentoring,
         message: "Success created mentoring request",
       }),
-      { status: 200 }
+      { status: 200 },
     );
   } catch (error) {
     console.error("Session Retrieval Error:", error);
     return NextResponse.json(
       { message: "Internal server error", error },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
@@ -111,13 +111,13 @@ export async function GET(req: NextRequest) {
         data: formattedData,
         message: "Success get Mentoring Requests",
       }),
-      { status: 200 }
+      { status: 200 },
     );
   } catch (error) {
     console.error("Session Retrieval Error:", error);
     return NextResponse.json(
       { message: "Internal server error", error },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
